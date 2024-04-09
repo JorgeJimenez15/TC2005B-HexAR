9f90ae064a9e8bf0a5ea86a07985a2e58bd6a481
Jorge Jiménez <github@jorgejimenez.net>
2024-04-08 19:05:20 -0600

    Add Prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/** @type {import('prettier').Config} */
+export default {
+	plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
+	overrides: [
+		{
+			files: "*.svelte",
+			options: {
+				parser: "svelte",
+			},
+		},
+	],
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: true,
+};
--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <h1 class="text-3xl text-green-500">Hello, World!</h1>
 <p class="text-slate-900">
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, ut accusantium distinctio beatae voluptates officiis repellendus vitae eos! Accusamus iste quas maxime quibusdam ratione,
-	voluptatum culpa quis quos sapiente ab!
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, ut accusantium distinctio beatae voluptates
+	officiis repellendus vitae eos! Accusamus iste quas maxime quibusdam ratione, voluptatum culpa quis quos sapiente
+	ab!
 </p>
