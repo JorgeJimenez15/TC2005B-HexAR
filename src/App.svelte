e19dd31ccec19af298b7f71f9d9f688711f0f42a
Jorge Jiménez <github@jorgejimenez.net>
2024-04-08 18:50:12 -0600

    Initialize project

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<h1>Hello, World!</h1>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, ut accusantium distinctio beatae voluptates officiis repellendus vitae eos! Accusamus iste quas maxime quibusdam ratione,
+	voluptatum culpa quis quos sapiente ab!
+</p>
