43039e07cea2b69e9428f7fcff8643d14b71c537
Jorge Jiménez <github@jorgejimenez.net>
2024-04-10 07:52:16 -0600

    Refactor Hit Testing and its reticle

--- src/Engine/main.ts
+++ src/Engine/main.ts
@@ -3,10 +3,10 @@ import { Scene, HemisphereLight, PerspectiveCamera, RingGeometry, MeshBasicMater
 export default class {
 	private scene: Scene;
 	private camera: PerspectiveCamera;
-	private reticle: Mesh;
 	private renderer: WebGLRenderer;
 	private session: XRSession | null;
 	private hitTest: {
+		reticle: Mesh;
 		requested: boolean;
 		source: XRHitTestSource | null;
 	};
@@ -20,20 +20,10 @@ export default class {
 		const light = new HemisphereLight(0xffffff, 0xbbbbff);
 		this.scene.add(light);
 
-		// * Camera
+		// * Perspective Camera
 		this.camera = new PerspectiveCamera(70, innerWidth / innerHeight, 0.01, 20);
 
-		// * Reticle
-		const geometry = new RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2);
-		const material = new MeshBasicMaterial();
-
-		this.reticle = new Mesh(geometry, material);
-		this.reticle.matrixAutoUpdate = false;
-		this.reticle.visible = false;
-
-		this.scene.add(this.reticle);
-
-		// * Renderer
+		// * WebGL Renderer
 		this.renderer = new WebGLRenderer({
 			alpha: true,
 			antialias: true,
@@ -44,14 +34,22 @@ export default class {
 		this.renderer.xr.enabled = true;
 		this.renderer.setAnimationLoop(this.update.bind(this));
 
-		// * Session
+		// * XR Session
 		this.session = null;
 
 		// * Hit test
+		const geometry = new RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2);
+		const material = new MeshBasicMaterial();
+
 		this.hitTest = {
+			reticle: new Mesh(geometry, material),
 			requested: false,
 			source: null,
 		};
+		this.hitTest.reticle.matrixAutoUpdate = false;
+		this.hitTest.reticle.visible = false;
+
+		this.scene.add(this.hitTest.reticle);
 
 		// * Event listeners
 		window.addEventListener("resize", this.onResize.bind(this));
@@ -71,39 +69,38 @@ export default class {
 		if (this.session) await this.session.end();
 	}
 
+	private requestHitTest(): void {
+		if (!this.session) return;
+
+		this.session.requestReferenceSpace("viewer").then((space) => {
+			this.session!.requestHitTestSource!({ space })?.then((source) => {
+				this.hitTest.source = source;
+			});
+		});
+
+		this.session.addEventListener("end", () => {
+			this.hitTest.requested = false;
+			this.hitTest.source = null;
+		});
+
+		this.hitTest.requested = true;
+	}
+
 	private update(timestamp: number, frame: XRFrame): void {
 		if (!frame) return;
-
-		const referenceSpace = this.renderer.xr.getReferenceSpace();
-
-		// Only runs once
-		if (!this.hitTest.requested) {
-			this.session!.requestReferenceSpace("viewer").then((space) => {
-				this.session!.requestHitTestSource!({ space })?.then((source) => {
-					this.hitTest.source = source;
-				});
-			});
-
-			this.session!.addEventListener("end", () => {
-				this.hitTest = {
-					requested: false,
-					source: null,
-				};
-			});
-
-			this.hitTest.requested = true;
-		}
+		if (!this.hitTest.requested) this.requestHitTest();
 
 		if (this.hitTest.source) {
 			const results = frame.getHitTestResults(this.hitTest.source);
 
 			if (results.length) {
-				const hit = results[0];
+				const referenceSpace = this.renderer.xr.getReferenceSpace()!;
+				const matrix = results[0].getPose(referenceSpace)!.transform.matrix;
 
-				this.reticle.visible = true;
-				this.reticle.matrix.fromArray(hit.getPose(referenceSpace!)!.transform.matrix);
+				this.hitTest.reticle.matrix.fromArray(matrix);
+				this.hitTest.reticle.visible = true;
 			} else {
-				this.reticle.visible = false;
+				this.hitTest.reticle.visible = false;
 			}
 		}
 
